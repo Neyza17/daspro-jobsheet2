7f9d938209f97b31b6cb72e2001e21c4c6df1898
Neyza17 <nesyaratuanastasya@gmail.com>
2024-09-07 13:02:21 +0700

    menambah biodata

---
+++ BiodataNajwa.java
@@ -0,0 +1,5 @@
+public class BiodataNajwa {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ BiodataNeyza.java
@@ -0,0 +1,5 @@
+public class BiodataNeyza {
+    public static void main(String[] args) {
+
+    }
+}
