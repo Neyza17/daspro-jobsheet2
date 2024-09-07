df2628bbe194bb3002497a4e0624868bd0ed7eb7
Najwah12 <13jujuenes@gmail.com>
2024-09-07 13:25:41 +0700

    Update BiodataNeyza.java
    Mengubah Biodata

--- BiodataNeyza.java
+++ BiodataNeyza.java
@@ -1,5 +1,8 @@
 public class BiodataNeyza {
     public static void main(String[] args) {
-
+        System.out.println("Nama    : Neyza Ratu A");
+        System.out.println("Nim     : 244107060119");
+        System.out.println("Kelas   : SIB 1C");
+        System.out.println("Hobby   : Berenang");
     }
 }
