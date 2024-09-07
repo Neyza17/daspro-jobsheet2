26da4c75299c1b536e9684563d5781ea64259bbf
Najwah12 <13jujuenes@gmail.com>
2024-09-07 13:21:27 +0700

    Update BiodataNajwah.java
    Menambah Biodata

--- BiodataNajwa.java
+++ BiodataNajwa.java
@@ -1,5 +1,8 @@
 public class BiodataNajwa {
     public static void main(String[] args) {
-
+        System.out.println("Nama    : Najwah Syihab");
+        System.out.println("Nim     : 244107060068");
+        System.out.println("Kelas   : SIB 1C");
+        System.out.println("Hobby   : Menyanyi");
     }
 }
