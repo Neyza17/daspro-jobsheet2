86b841144d14b701d134b621731e4c0d9f71f9c2
Najwah12 <13jujuenes@gmail.com>
2024-09-07 13:29:48 +0700

    Update BiodataSharon.java
    Mengubah Biodata

--- BiodataSharon.java
+++ BiodataSharon.java
@@ -1,5 +1,8 @@
 public class BiodataSharon {
     public static void main(String[] args) {
-
+        System.out.println("Nama    : Sharon Avrilunajwa Sajidah");
+        System.out.println("Nim     : 244107060017");
+        System.out.println("Kelas   : SIB 1C");
+        System.out.println("Hobby   : Menyanyi");
     }
 }
